6973f70e298b5cbb079c98d82f6b99b6565a3c8c
Simone <simoneugazio1993@gmail.com>
2019-10-16 17:12:02 +0200

    2 push

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var nome = prompt ("scrivi qui il tuo nome");
+
+console.log(nome);
+
+var ciao = "ciao " + prompt;
+
+console.log(ciao);
+
+var cognome = prompt ("mi serve anche il tuo cognome")
+
+console.log(cognome);
+
+var colore = prompt ("ora il tuo colore preferito")
+
+console.log(colore);
